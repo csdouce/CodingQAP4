a40f6719f2eab252ccef1c88662d185c8d4aef29
csdouce <chris.doucette@keyin.com>
2022-11-22 18:27:36 -0400

    Completed the Shape and Ellipse classes

--- src/main/java/com/keyin/qap4/problem1/Ellipse.java
+++ src/main/java/com/keyin/qap4/problem1/Ellipse.java
@@ -16,8 +16,8 @@ public class Ellipse  extends Shape{
         this.axesB = 1.0;
     }
 
-    public Ellipse(String color, boolean isFilled, double firstAxes, double secondAxes){
-        super(color, isFilled);
+    public Ellipse(String color, boolean isFilled, String name, double firstAxes, double secondAxes){
+        super(color, isFilled, name);
 
         if(secondAxes > firstAxes){
             this.axesA = secondAxes;
@@ -28,15 +28,42 @@ public class Ellipse  extends Shape{
         }
     }
 
+    // Getters and Setters
+    public double getAxesA() {
+        return axesA;
+    }
+
+    public void setAxesA(double axesA) {
+        this.axesA = axesA;
+    }
+
+    public double getAxesB() {
+        return axesB;
+    }
+
+    public void setAxesB(double axesB) {
+        this.axesB = axesB;
+    }
+
     @Override
     public double getArea(){
         // Area = PIE * a * b
-        return 1.0;
+        return Math.PI * this.axesA * this.axesB;
     }
 
     @Override
     public double getPerimeter(){
+        double perimeter;
 
-        return 1.0;
+        if (this.axesA == this.axesB){
+            // Both axes are the same so formula would be same as a Circle
+            perimeter = 2 * Math.PI * this.axesA;
+        } else {
+            // Formula for an Ellipse
+            perimeter = Math.PI * Math.sqrt(2 * (Math.pow(this.axesA, 2) + Math.pow(this.axesB, 2) - Math.pow(this.axesA - this.axesB, 2)/2));
+        }
+        return perimeter;
     }
+
+
 }
