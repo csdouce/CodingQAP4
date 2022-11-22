10c294bd24e7a15eb64665cd19beabbf0cac19f5
csdouce <chris.doucette@keyin.com>
2022-11-22 10:58:24 -0400

    Creating the Shape and Ellipse Classes

--- src/main/java/com/keyin/qap4/problem1/Ellipse.java
+++ src/main/java/com/keyin/qap4/problem1/Ellipse.java
@@ -6,9 +6,37 @@ package com.keyin.qap4.problem1;
 
 public class Ellipse  extends Shape{
     // Instance Variables
-    private int axesA;
-    private int axesB;
+    private double axesA;
+    private double axesB;
 
     // Constructors
+    public Ellipse(){
+        super();
+        this.axesA = 1.5;
+        this.axesB = 1.0;
+    }
 
+    public Ellipse(String color, boolean isFilled, double firstAxes, double secondAxes){
+        super(color, isFilled);
+
+        if(secondAxes > firstAxes){
+            this.axesA = secondAxes;
+            this.axesB = firstAxes;
+        } else {
+            this.axesA = firstAxes;
+            this.axesB = secondAxes;
+        }
+    }
+
+    @Override
+    public double getArea(){
+        // Area = PIE * a * b
+        return 1.0;
+    }
+
+    @Override
+    public double getPerimeter(){
+
+        return 1.0;
+    }
 }
